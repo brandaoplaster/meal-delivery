bb4f4ed150356494156e0ad2e42c6f0f23bd255a
brandaoplaster <brandaoplaster@gmail.com>
2021-05-29 23:43:24 -0300

    feat: Adding model profile and created tests

---
+++ spec/models/profile_spec.rb
@@ -0,0 +1,9 @@
+require 'rails_helper'
+
+RSpec.describe Profile, type: :model do
+  it { is_expected.to validate_presence_of(:name) }
+  it { is_expected.to validate_presence_of(:phone) }
+  it { is_expected.to validate_uniqueness_of(:phone) }
+
+  it { is_expected.to belong_to :user }
+end
